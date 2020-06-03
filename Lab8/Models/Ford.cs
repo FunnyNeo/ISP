bccfcf7f5eca43582153b61d3d72e80751c497d8
FunnyNeo <vano-vora@mail.ru>
2020-06-03 15:44:53 +0300

    8 лаба

---
+++ Lab8/Models/Ford.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Cars.Models
+{
+    //Класс содержащий аргументы
+    public class ColorEventArgs : EventArgs
+    {
+        public ColorEventArgs(string color)
+        {
+            Color = color;
+        }
+
+        public string Color;
+    }
+
+    public class Ford : Car
+    {
+        public string currentColor;
+        
+        //События
+        public event EventHandler<ColorEventArgs> ColorChanged;
+
+        protected virtual void OnColorChanged(ColorEventArgs e)
+        {
+            ColorChanged?.Invoke(this, e);//Invoke вызывает все подписанные события с параметрами которые есть в Invoke
+        }
+
+        public Ford(string name, List<string> colors, int horsePower, string carType, int cost) : base(name, Model.Ford,
+            colors, horsePower, carType, cost)
+        {
+            currentColor = colors[0];
+            ColorChanged += (s, e) =>
+                Console.WriteLine($"Old color: {((Ford) s).currentColor}\nNew color: {e.Color}");
+        }
+
+        //Вызывает Event
+        public void ChangeColor(int i)
+        {
+            OnColorChanged(new ColorEventArgs(Colors[i]));
+        }
+    }
+}
---
+++ Lab8/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Cars.Models;
+
+namespace Cars
+{
+    public delegate void GetName(Car c);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            cars.Add(new Bmw("Bmw Car", new List<string>(new []{"black", "white"}), 250, "Offroad", 2500));
+            cars.Add(new Ford("Ford Car", new List<string>(new []{"black", "white"}), 400, "City", 4500));
+            cars.Add(new Ferrari("Ferrari Car", new List<string>(new []{"black", "white"}), 600, "City", 9500));
+            cars.Add(new Volkswagen("Volkswagen Car", new List<string>(new []{"black", "white"}), 320, "Offroad", 3500));
+            cars.Add(new Audi("Audi Car", new List<string>(new []{"black", "white"}), 300, "Offroad", 1500));
+            cars.Add(new Opel("Opel Car", new List<string>(new []{"black", "white"}), 250, "City", 1500));
+            cars.Add(new Porsche("Porsche Car", new List<string>(new []{"black", "white"}), 150, "City", 5500));
+
+            foreach (Car c in cars)
+            {
+                c.SlapRoof();
+                Console.WriteLine($"{c}\n");
+            }
+            
+            
+            // Events
+            Ford f = new Ford("Ford Car", new List<string>(new[] {"black", "white"}), 400, "City", 4500);
+            
+            f.ChangeColor(0);
+            f.ChangeColor(1);
+
+            //подписываем функцию ColorChanged
+            f.ColorChanged += (s, e) => Console.WriteLine($"Смена цвета на: {e.Color} в {((Ford) s).Name} / лямбда - выражение");
+
+
+            f.ChangeColor(0);
+            f.ChangeColor(1);
+            //Делегат
+            f.ColorChanged += delegate(object? s, ColorEventArgs e)
+            {
+                Console.WriteLine($"Смена цвета на: {e.Color} в {((Ford) s).Name} / анонимной функции");
+            };
+            
+            f.ChangeColor(0);
+            f.ChangeColor(1);
+            
+            // Анонимная функция
+            GetName gn = delegate(Car c)
+            {
+                Console.WriteLine($"{c.Name} / анонимная функция");
+            };
+            
+            // Лямбда - выражение
+            GetName gnAnonymous = car => Console.WriteLine($"{car.Name} / лямбда - выражение");
+            
+            foreach (Car c in cars)
+            {
+                gn(c);
+                gnAnonymous(c);
+            }
+            
+            // Исключения
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    int _ = 0 / i;
+                }
+                catch (ArithmeticException ex)
+                {
+                    // Арифметиеское исключение
+                    Console.WriteLine($"Division error by: {i}");
+                }
+                catch
+                {
+                    // Пустой catch, который ловит все что не было выше
+                    Console.WriteLine("I dont know wtf is happende");
+                }
+                finally
+                {
+                    //Выполняется всегда
+                    Console.WriteLine($"current number {i}");
+                }
+            }
+        }
+    }
+}
